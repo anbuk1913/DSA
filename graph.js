b67e251fa0a8ffb55a29c0f7eee0a5252b9ac666
anbuk143 <anbukumar1913@gmail.com>
2025-06-07 12:13:28 +0530

    graph

--- graph.js
+++ graph.js
@@ -3,40 +3,40 @@ class Graph{
         this.adjacency = {}
     }
     addVertex(vertex){
-        if(!this.adjacency[vertex]){
-            this.adjacency[vertex] = new Set()
+        if(!this.addVertex[vertex]){
+            this.addVertex[vertex] = new Set()
         }
     }
     addEdge(vertex1,vertex2){
         if(!this.adjacency[vertex1]){
             this.addVertex(vertex1)
         }
-        if(this.adjacency[vertex2]){
+        if(!this.adjacency[vertex2]){
             this.addVertex(vertex2)
         }
-        this.adjacency[vertex1].add(vertex2)
+        this.addVertex[vertex1].add(vertex2)
         this.adjacency[vertex2].add(vertex1)
     }
     display(){
-        for(i in this.adjacency){
-            console.log(i +" -> "+[...this.adjacency[i]])
+        for(let i in this.adjacency){
+            console.log(i+" -> "+[...this.adjacency[i]])
         }
     }
     hasEdge(vertex1,vertex2){
-        return this.adjacency[vertex1].has(vertex2) && this.adjacency[vertex2].has(vertex1)
+        return this.adjacency[vertex1].has(vertex2) && this.addVertex[vertex2].has(vertex2)
     }
-    removeEdge(vertex1,vertex2){
-        this.removeEdge[vertex1].delete(vertex2)
-        this.removeEdge[vertex2].delete(vertex1)
+    removeEgde(vertex1,vertex2){
+        this.adjacency[vertex1].delete(vertex2)
+        this.adjacency[vertex2].delete(vertex1)
     }
     removeVertex(vertex){
         if(!this.adjacency[vertex]){
-            return
+            return null
         } else {
             for(let i of this.adjacency[vertex]){
-                this.removeEdge(i,vertex)
+                this.removeEgde(i,vertex)
             }
-            delete this.adjacency(vertex)
+            delete this.adjacency[vertex]
         }
     }
 }
